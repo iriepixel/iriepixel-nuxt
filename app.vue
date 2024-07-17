2bf7caf2f30bceef18aeb097e0918e723c16f61a
Jev IRIE PIXEL <jev@iriepixel.com>
2024-07-17 16:07:50 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,12 @@
+<template>
+    <NuxtRouteAnnouncer />
+    <Head>
+      <Title>IRIE PIXEL - JS, React, Vue</Title>
+      <Meta name="description" content="Freelance JAMstack Frontend Web Developer - JavaScript, React, Vue, Next, Nuxt - Brighton / London / Remote"/>
+    </Head>
+    <div class="flex items-center justify-center h-screen bg-black">
+      <div class=" w-[300px] h-[300px]">
+        <img class="" src="/under-construction.gif" />
+      </div>
+    </div>
+</template>
