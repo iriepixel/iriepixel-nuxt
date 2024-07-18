734035d900c5ef85e0014b67a324cfb9f6c16aee
Jev IRIE PIXEL <jev@iriepixel.com>
2024-07-18 11:39:01 +0100

    homepages under construction update

--- app.vue
+++ app.vue
@@ -4,9 +4,16 @@
       <Title>IRIE PIXEL - JS, React, Vue</Title>
       <Meta name="description" content="Freelance JAMstack Frontend Web Developer - JavaScript, React, Vue, Next, Nuxt - Brighton / London / Remote"/>
     </Head>
-    <div class="flex items-center justify-center h-screen bg-black">
+    <div class="flex flex-col items-center justify-center h-screen text-white bg-black">
       <div class=" w-[300px] h-[300px]">
         <img class="" src="/under-construction.gif" />
       </div>
+      <h1 class="my-4 text-[20px] font-bold">IRIE PIXEL</h1>
+      <div class="text-center max-w-[400px] mb-6">
+        <p class="">Freelance JAMstack Frontend Web Developer</p>
+        <p class="">JavaScript, React, Vue, Next, Nuxt</p>
+        <p class="">Brighton / London / Remote</p>
+      </div>
+      <a class="underline" href="mailto:jev@iriepixel.com">send me email</a>
     </div>
 </template>
