762cc2014514c49c99ceea7104d34d31b42b64d3
Jev IRIE PIXEL <jev@iriepixel.com>
2024-11-05 17:21:44 +0000

    content updates

--- app.vue
+++ app.vue
@@ -1,19 +1,7 @@
 <template>
-    <NuxtRouteAnnouncer />
-    <Head>
-      <Title>IRIE PIXEL - JS, React, Vue</Title>
-      <Meta name="description" content="Freelance JAMstack Frontend Web Developer - JavaScript, React, Vue, Next, Nuxt - Brighton / London / Remote"/>
-    </Head>
-    <div class="flex flex-col items-center justify-center h-screen text-white bg-black">
-      <div class=" w-[300px] h-[300px]">
-        <img class="" src="/under-construction.webp" />
-      </div>
-      <h1 class="my-4 text-[20px] font-bold">IRIE PIXEL</h1>
-      <div class="text-center max-w-[400px] mb-6">
-        <p class="">Freelance JAMstack Frontend Web Developer</p>
-        <p class="">JavaScript, React, Vue, Next, Nuxt</p>
-        <p class="">Brighton / London / Remote</p>
-      </div>
-      <a class="underline" href="mailto:jev@iriepixel.com">send me email</a>
-    </div>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
 </template>
