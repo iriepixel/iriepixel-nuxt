7e9ed29dbaf22f0ee755ce3bf16afa6c5cfbaf35
Jev IRIE PIXEL <jev@iriepixel.com>
2024-11-05 17:27:05 +0000

    nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/image"],
   devtools: { enabled: true },
-  compatibilityDate: "2024-11-05"
-})
+  modules: ["@nuxt/image", "@nuxtjs/tailwindcss"],
+});
