7731bdc23e0e35a3a2bfe2fef6abc8dbdce8c9de
Jev IRIE PIXEL <jev@iriepixel.com>
2024-11-05 16:52:41 +0000

    nuxt updated to 3.14.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/image", "@nuxtjs/tailwindcss"]
+  modules: ["@nuxt/image", "@nuxtjs/tailwindcss"],
+  compatibilityDate: "2024-11-05"
 })
